ed7200cc531c3b0a25b25246f3820eb4882e9259
ramdaniAlghifary <ramdanialghifary44@gmail.com>
2025-07-08 11:24:28 +0700

    proses.php

--- proses.php
+++ proses.php
@@ -9,19 +9,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $jumlah = (int) $_POST["jumlah"];
     $kategori = $_POST["kategori"];
     $total = hitung_total($jumlah, $kategori);
-    echo "<pre>";
-    echo "+-----------------------------+\n";
-    echo "|       Detail Pemesanan      |\n";
-    echo "+-----------------------------+\n";
-
-// Isi data dengan rata kiri dan padding
-    printf("| %-12s : %-12s |\n", "Nama", $nama);
-    printf("| %-12s : %-12s |\n", "Jumlah Tiket", $jumlah);
-    printf("| %-12s : %-12s |\n", "Kategori", $kategori);
-    printf("| %-12s : %-12s |\n", "Total Harga", $total);
-
-    echo "+-----------------------------+\n";
-    echo " TERIMA KASIH ATAS PEMESANAN NYA";
-    echo "<?pre>";
-}
 ?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Nota Pemesanan</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="output-page">
+
+<div class="nota-container">
+    <h2>Pemesanan Tiket Berhasil</h2>
+    <h3>Nota Pemesanan</h3>
+    
+    <p>Nama: <?= $nama ?></p>
+    <p>jumlah:  <?= $jumlah ?></p>
+    <p>kategori: <?= $kategori ?></p>
+    
+    <hr style="border: 0.5px dotted #aaa; margin: 20px 0;">
+    
+    <div class="total">Total Bayar: Rp <?= number_format($total, 0, ',', '.') ?></div>
+    
+    <a href="form.html" class="btn-kembali">🔙 Kembali ke Form</a>
+</div>
+
+</body>
+</html>
+
